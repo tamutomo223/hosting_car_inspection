c0a4b194807c27842220ffcca9f638b8e4da5b5d
tamutomo223 <57092560+tamutomo223@users.noreply.github.com>
2020-07-12 23:07:08 +0900

    no message

--- sw.js
+++ sw.js
@@ -28,5 +28,5 @@ workbox.precaching.cleanupOutdatedCaches()
 // --------------------------------------------------
 
 // Register route handlers for runtimeCaching
-workbox.routing.registerRoute(new RegExp('/nuxt2/'), new workbox.strategies.CacheFirst ({}), 'GET')
-workbox.routing.registerRoute(new RegExp('/'), new workbox.strategies.NetworkFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/hosting_car_inspection/nuxt2/'), new workbox.strategies.CacheFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/hosting_car_inspection/'), new workbox.strategies.NetworkFirst ({}), 'GET')
